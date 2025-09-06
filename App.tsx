27aa9403d2a898fbecb295eb2b58336685c108b7
Vivian Nnolum <viviannnolum23@gmail.com>
2025-09-06 11:29:50 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import Alert from "./components/Alert";
+
+function App() {
+  return (
+    <div>
+      <Alert>
+        Hello world
+      </Alert> 
+    </div>
+  );
+}
+
+export default App;
---
+++ Message.tsx
@@ -0,0 +1,9 @@
+
+function  Messsage(){
+    const name = 'Vee';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Messsage
